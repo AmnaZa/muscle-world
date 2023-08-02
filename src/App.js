e07136c47b5ba09e017fc0d1bc48d5d39884f790
AmnaZa <113046745+AmnaZa@users.noreply.github.com>
2023-08-02 01:31:15 -0400

    Nav bar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-
+import { Box } from '@mui/material';
 import './App.css';
+import Navbar from './components/Navbar';
 
 function App() {
-  return 
-    
-
+  return (
+      <Navbar />
+      
+  );
 }
-
 export default App;

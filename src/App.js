36d70c674516bbbf38b2e198fa1cb0d33f1348e8
AmnaZa <113046745+AmnaZa@users.noreply.github.com>
2023-08-02 09:53:08 -0400

    fix error in nav bar and footer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import { Box } from '@mui/material';
 import './App.css';
-import Navbar from './components/Navbar';
+import  Navbar from './components/Navbar';
+import  Footer from './components/Footer';
+
 
 function App() {
   return (
+    <div>
       <Navbar />
-      
-  );
+      <Footer />
+  
+    </div>
+  )
 }
+
 export default App;
